f50ed0a4c310caebd0ad3ae7b072cafa9e32a1f4
Mihir160 <mihir.roy266@gmail.com>
2022-10-26 14:42:37 +0600

    course add  server

--- index.js
+++ index.js
@@ -1,11 +1,24 @@
 const express = require('express')
 const app = express()
 const port = process.env.PORT || 5000;
-
+const courses = require('./data/programming-language.json')
+const cors = require('cors')
+app.use(cors())
 app.get('/',(req, res) =>{
     res.send('API Running')
 })
 
+app.get('/courses', (req, res) =>{
+    res.send(courses)
+})
+
+app.get('/course/:id',(req, res)=>{
+    const id = req.params.id
+    const selectedCourse = courses.find(n => n.courseName_id === id)
+    res.send(selectedCourse)
+    console.log(req.params)
+})
 app.listen(port, () => {
     console.log('Server running on port test', port)
 })
+
